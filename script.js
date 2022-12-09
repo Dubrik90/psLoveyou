9f7cc81a12572ab588958f662ebfbb43a093a893
alex <Dubik90@yandex.by>
2022-12-09 20:05:22 +0300

    3 commit

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
 "use strict"
 const prev = document.getElementById('btn-prev'),
-      next = document.getElementById('btn-next'),
-      slids = document.querySelectorAll('.slide'),
-      dotWrapper = document.querySelector('.dots-wrapper');
-      
-      
-console.log(dotWrapper);
+    next = document.getElementById('btn-next'),
+    slids = document.querySelectorAll('.slide'),
+    dotWrapper = document.querySelector('.dots-wrapper');
+
 
 let index = 0;
 let outPut = '';
 
 slids.forEach((data, index) => {
-    if (data) {
+    if (data && index === 0) {
+        outPut += `
+        <p class="dot active"></p>`
+    } else {
         outPut += `
         <p class="dot"></p>`
     }
 });
+
 dotWrapper.innerHTML = outPut;
+
 const dots = document.querySelectorAll('.dot');
-const activeSlide = n => {
-    for(let slide of slids) {
+
+const activeSlide = (n) => {
+    for (let slide of slids) {
         slide.classList.remove('active');
     }
     slids[n].classList.add('active');
 };
 
-const activeDot = n => {
-    for(let dot of dots) {
+const activeDot = (n) => {
+    for (let dot of dots) {
         dot.classList.remove('active');
     }
     dots[n].classList.add('active');
 };
 
-const prepareCurrentSlide = ind => {
-        activeSlide(index);
-        activeDot(index);
-    }
+
+const prepareCurrentSlide = (ind) => {
+    activeSlide(index);
+    activeDot(index);
+}
 
 const nextSlide = () => {
-    if(index == slids.length - 1) {
+    if (index == slids.length - 1) {
         index = 0;
         prepareCurrentSlide(index);
-       } else {
+    } else {
         index++;
         prepareCurrentSlide(index);
     };
- 
-
 }
 
 const prevSlide = () => {
-    if(index == 0) {
+    if (index == 0) {
         index = slids.length - 1;
         prepareCurrentSlide(index);
-       
     } else {
         index--;
         prepareCurrentSlide(index);
     };
-
 }
 
 dots.forEach((item, indexDot) => {
@@ -72,6 +73,4 @@ next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
 
 
-const interval = setInterval(() => nextSlide(), 20000);
- 
-
+const interval = setInterval(() => nextSlide(), 300000);
